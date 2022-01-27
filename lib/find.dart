eee340392727181bd9b432fa8f30abbfb92fa71b
Yoonsoo Kim <laptise@live.jp>
2022-01-27 20:34:40 +0900

    ユーザー検索機能

--- lib/find.dart
+++ lib/find.dart
@@ -5,14 +5,49 @@ class FindTop extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final double deviceHeight = MediaQuery.of(context).size.height;
     return Scaffold(
         body: Column(
       children: [
         SizedBox(
-          child: Column(
-            children: [
-              const Text("プレミアムユーザーを探す"),
-            ],
+          child: TextButton(
+            child: const Text(
+              "プレミアムユーザーを探す",
+              style: TextStyle(color: Colors.black),
+            ),
+            onPressed: () {
+              showModalBottomSheet<void>(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return SizedBox(
+                        height: deviceHeight * 1,
+                        child: Container(
+                            child: Column(
+                          children: [
+                            Container(
+                              width: double.infinity,
+                              decoration: BoxDecoration(color: Colors.amber),
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text('ユーザーを検索',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black,
+                                      fontSize: 18.0,
+                                    )),
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.all(10),
+                              child: Column(
+                                children: [TextFormField()],
+                              ),
+                            )
+                          ],
+                        )));
+                  });
+            },
           ),
         )
       ],
