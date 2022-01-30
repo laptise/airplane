e11fc02ca5cad8983441fec2470ace0fa5627c4c
Yoonsoo Kim <laptise@live.jp>
2022-01-30 18:53:56 +0900

    premium user menu

--- lib/premiums/manage_menu.dart
+++ lib/premiums/manage_menu.dart
@@ -1,8 +1,30 @@
 import 'package:flutter/material.dart';
 
 class ManageTop extends StatelessWidget {
+  const ManageTop({Key? key}) : super(key: key);
+
+  Widget buildBlock(BuildContext context, IconData icon, String label) {
+    return Expanded(
+      flex: 1,
+      child: AspectRatio(
+        aspectRatio: 1,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [Icon(icon), Text(label)],
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text("karni"));
+    return Scaffold(
+        body: Row(children: [
+      buildBlock(context, Icons.manage_accounts, "道具１"),
+      buildBlock(context, Icons.headphones, "道具２"),
+      buildBlock(context, Icons.library_add, "道具３"),
+    ]));
   }
 }
